a744d28dd985622b83fdf4149fa277f31c1d40d0
Davi <andrielysom@gmail.com>
2025-08-26 19:14:59 -0300

    juci

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/presentes", (req, res) => {
     return res.status(400).send("Dados incompletos");
   }
 
-  const sql = "SELECT nome, data_presente, presente_escolhido FROM presentes";
+  const sql = "INSERT INTO presentes (nome, data_presente, presente_escolhido) VALUES (?, ?, ?)";
   db.query(sql, [nome, data_presente, presente_escolhido], (err, result) => {
     if (err) {
       console.error("Erro ao inserir:", err);
